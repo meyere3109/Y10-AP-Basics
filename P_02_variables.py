55488301d15c9c055adb2dc7d8b0b736c9dce070
meyere3109 <meyere3109@masseyhigh.school.nz>
2024-05-21 14:30:12 +1200

    Variable Basics

---
+++ P_02_variables.py
@@ -0,0 +1,12 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+print(fav_num*2)
+print(num_string*5)
+
