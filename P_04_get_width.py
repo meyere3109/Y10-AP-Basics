03b5cdc197108789e03c572f13a093a14b7b220e
meyere3109 <meyere3109@masseyhigh.school.nz>
2024-05-23 09:47:36 +1200

    Working number checking function

--- P_04_get_width.py
+++ P_04_get_width.py
@@ -1,14 +1,30 @@
-error = "Please enter a number that is more than zero\n"
-while True:
+# Ask user for width and loop until they
+# enter a number that is more than zero
+def num_check(question):
+    error = "Please enter a number that is more than zero\n"
+    while True:
 
-    try:
-    # Ask user for width and loop until they
-        width = float(input("Width: "))
+        try:
+            # Ask user for Response and loop until they
+            Response = float(input(question))
 
-    # enter a number that is more than zero
-        if width > 0:
-           break
-        else:
-           print(error)
-    except ValueError:
-        print (error)
+            # enter a number that is more than zero
+            if Response > 0:
+                return Response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# Main Routine Goes Here
+for item in range(0, 2):
+    width = num_check("Width: ")
+    print(width)
+
+    print()
+
+for item in range(0, 2):
+    height = num_check("Height: ")
+    print(height)
