45d253214d0077e0b9bf9cf8b4b94fb52dbafdf8
meyere3109 <meyere3109@masseyhigh.school.nz>
2024-05-23 09:35:11 +1200

    Gets width only

---
+++ P_04_get_width.py
@@ -0,0 +1,14 @@
+error = "Please enter a number that is more than zero\n"
+while True:
+
+    try:
+    # Ask user for width and loop until they
+        width = float(input("Width: "))
+
+    # enter a number that is more than zero
+        if width > 0:
+           break
+        else:
+           print(error)
+    except ValueError:
+        print (error)
